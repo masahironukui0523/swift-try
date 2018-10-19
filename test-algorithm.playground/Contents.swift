f199b1c826bf40a66f638629c8feadfa4fb194ee
masa <mickey24352002@gmail.com>
2018-10-19 16:23:03 +0900

    Commit.

--- test-algorithm.playground/Contents.swift
+++ test-algorithm.playground/Contents.swift
@@ -1,3 +1,29 @@
-import Cocoa
+import Foundation
 
-var str = "Hello, playground"
+var arrayN = [3, 2, 9, 8]
+
+var target = 10
+
+
+func ans(arr: [Int], tar: Int) -> (Int, Int)? {
+    
+    var tempArr = [Int: Int]()
+    
+    for (index, value)  in arr.enumerated() {
+        var ans = target - value
+        
+        if let ans = tempArr[ans] {
+            return (index, ans)
+        } else {
+            tempArr[value] = index
+            
+        }
+        
+    }
+    
+    
+    return nil
+    
+}
+
+print(ans(arr: arrayN, tar: target))
