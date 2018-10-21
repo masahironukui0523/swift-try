cda97fca081f2109f6916f21b09d874b4bf1e01b
masa <mickey24352002@gmail.com>
2018-10-22 00:17:22 +0900

    Advantages and disadvantages of each collection types.

---
+++ swift-basic/swift-standard-library.playground/Contents.swift
@@ -0,0 +1,20 @@
+import Cocoa
+
+// 1 Array
+
+// ordar
+var people = ["masa", "taka", "hiroshi"]
+
+// random access
+
+// Insertion location
+
+// this is a constant-time.
+people.append("risa")
+
+// this is a not constant-time
+people.insert("shin", at: 0)
+
+
+// 2 Dictionary
+
